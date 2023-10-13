05cb186a495cdb06a334afdf8a0855d368a9337d
nomorezhanje23 <nomorezhanje23@gmail.com>
2023-10-14 01:37:20 +0200

    Error handling, changed users in controllers

--- controllers/users.js
+++ controllers/users.js
@@ -1,22 +1,38 @@
 const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
-const getAll = async (req, res) => {
+
+
+const getAll =  (req, res) => {
     //swagger.tags=['Users']
-    const result = await mongodb.getDatabase().db().collection('users').find();
-    result.toArray().then((users) => {
+    mongodb
+    .getDatabase()
+    .db()
+    .collection('users')
+    .find()
+    .toArray((err, users) => {
+        if(err) {
+            res.status(400).json({message: err});
+        }
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(users);
     });
 };
 
-const getSingle = async (req, res) => {
+const getSingle = (req, res) => {
     //swagger.tags=['Users']
     const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('users').find({_id: userId});
-    result.toArray().then((users) => {
+    mongodb
+    .getDatabase()
+    .db()
+    .collection('users')
+    .find({_id: userId})
+    .toArray((err, result) => {
+        if (err) {
+            res.status(400).json({message: err});
+        }
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users[0]);
+        res.status(200).json(result[0]);
     });
 };
 
